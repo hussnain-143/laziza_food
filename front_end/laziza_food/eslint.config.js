0abb9a465527566d09185f3a0ccecc7a529c2563
Hussnain Ahmed <hussnainahmed143ha@gmail.com>
2025-05-20 17:16:42 +0500

    Set the admin Template

--- front_end/laziza_food/eslint.config.js
+++ front_end/laziza_food/eslint.config.js
@@ -1,16 +1,18 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import globals from 'globals'
 import js from '@eslint/js'
 import pluginVue from 'eslint-plugin-vue'
+import globals from 'globals'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
-export default defineConfig([
+export default [
   {
     name: 'app/files-to-lint',
     files: ['**/*.{js,mjs,jsx,vue}'],
   },
 
-  globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
+  {
+    name: 'app/files-to-ignore',
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
+  },
 
   {
     languageOptions: {
@@ -23,4 +25,4 @@ export default defineConfig([
   js.configs.recommended,
   ...pluginVue.configs['flat/essential'],
   skipFormatting,
-])
+]
--- front_end/laziza_food/src/App.vue
+++ front_end/laziza_food/src/App.vue
@@ -1,47 +1,50 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+<script>
+// ================= JS IMPORTS =================
+
+// Vendor JS
+// import '@/assets/vendors/js/vendor.bundle.base.js'
+// import '@/assets/vendors/chart.js/Chart.min.js'
+// import '@/assets/vendors/progressbar.js/progressbar.min.js'
+// import '@/assets/vendors/jvectormap/jquery-jvectormap.min.js'
+// import '@/assets/vendors/jvectormap/jquery-jvectormap-world-mill-en.js'
+// import '@/assets/vendors/owl-carousel-2/owl.carousel.min.js'
+
+// Custom scripts
+// import '@/assets/js/off-canvas.js'
+// import '@/assets/js/hoverable-collapse.js'
+// import '@/assets/js/misc.js'
+// import '@/assets/js/settings.js'
+// import '@/assets/js/todolist.js'
+// import '@/assets/js/dashboard.js'
+
+import Layout from './components/Layout/admin/Layout.vue';
+
+import Header from './components/main/admin/Header.vue';
+import Sidebar from './components/main/admin/Sidebar.vue';
+import Footer from './components/main/admin/Footer.vue';
+
+export default {
+  components: {
+    Layout,
+    Header,
+    Sidebar,
+    Footer,
+  }
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div class="container-scroller">
+    <Sidebar />
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <Header />
+      <!-- partial -->
+      <div class="main-panel">
+        <Layout />
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+        <Footer />
+      </div>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
